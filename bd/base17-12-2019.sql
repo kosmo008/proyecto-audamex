1da1ac75fda747fe586395dada0bbcfed5616211
Daniel Cibrian <kosmo.08america@gmail.com>
2019-12-18 10:58:42 -0600

    correccion a base de datos
    se agregaron autoincrementables a las llaves primarias y correccion de nomenclatura lower camel case

--- bd/base17-12-2019.sql
+++ bd/base17-12-2019.sql
@@ -18,7 +18,7 @@ USE `Audamex` ;
 -- Table `Audamex`.`cliente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`cliente` (
-  `idCliente` INT NOT NULL,
+  `idCliente` INT NOT NULL AUTO_INCREMENT,
   `expediente` VARCHAR(45) NULL,
   `razonSocial` VARCHAR(200) NULL,
   `noPersonas` INT NULL,
@@ -38,7 +38,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`norma`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`norma` (
-  `idNorma` INT NOT NULL,
+  `idNorma` INT NOT NULL AUTO_INCREMENT,
   `acronimo` VARCHAR(10) NULL,
   `claveMex` VARCHAR(90) NULL,
   `claveISO` VARCHAR(45) NULL,
@@ -52,7 +52,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`representate`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`representate` (
-  `idRepresentate` INT NOT NULL,
+  `idRepresentate` INT NOT NULL AUTO_INCREMENT,
   `puesto` VARCHAR(90) NULL,
   `nombre` VARCHAR(90) NULL,
   `telefono` VARCHAR(10) NULL,
@@ -74,7 +74,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`certificacion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`certificacion` (
-  `idCertificacion` INT NOT NULL,
+  `idCertificacion` INT NOT NULL AUTO_INCREMENT,
   `alcance` LONGTEXT NULL,
   `noCertificado` VARCHAR(60) NULL,
   `diasAuditoria` INT NULL,
@@ -95,7 +95,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`sitio`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`sitio` (
-  `idSitio` INT NOT NULL,
+  `idSitio` INT NOT NULL AUTO_INCREMENT,
   `noSitio` INT NULL,
   `descripcion` VARCHAR(90) NULL,
   `referencia` VARCHAR(120) NULL,
@@ -125,7 +125,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`turno`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`turno` (
-  `idTurno` INT NOT NULL,
+  `idTurno` INT NOT NULL AUTO_INCREMENT,
   `descripcion` VARCHAR(90) NULL,
   `condicion` TINYINT NULL,
   PRIMARY KEY (`idTurno`))
@@ -136,22 +136,22 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`proceso`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`proceso` (
-  `idProceso` INT NOT NULL,
+  `idProceso` INT NOT NULL AUTO_INCREMENT,
   `descripcion` VARCHAR(90) NULL,
-  `InternosExternos` VARCHAR(45) NULL,
+  `internosExternos` VARCHAR(45) NULL,
   `condicion` TINYINT NULL,
   `sitio_idSitio` INT NOT NULL,
-  `turno_idTurno1` INT NOT NULL,
+  `turno_idTurno` INT NOT NULL,
   PRIMARY KEY (`idProceso`),
   INDEX `fk_proceso_sitio1_idx` (`sitio_idSitio` ASC),
-  INDEX `fk_proceso_turno1_idx` (`turno_idTurno1` ASC),
+  INDEX `fk_proceso_turno1_idx` (`turno_idTurno` ASC),
   CONSTRAINT `fk_proceso_sitio1`
     FOREIGN KEY (`sitio_idSitio`)
     REFERENCES `Audamex`.`sitio` (`idSitio`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_proceso_turno1`
-    FOREIGN KEY (`turno_idTurno1`)
+    FOREIGN KEY (`turno_idTurno`)
     REFERENCES `Audamex`.`turno` (`idTurno`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
@@ -162,21 +162,21 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`cotizacion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`cotizacion` (
-  `idCotizacion` INT NOT NULL,
+  `idCotizacion` INT NOT NULL AUTO_INCREMENT,
   `fecha` DATE NULL,
   `descripcion` LONGTEXT NULL,
   `fechaProbAud` DATE NULL,
   `fechaImpSG` DATE NULL,
-  `Subtotal` DECIMAL(12,2) NULL,
+  `subtotal` DECIMAL(12,2) NULL,
   `costoSeg` DECIMAL(12,2) NULL,
   `costoDesc` DECIMAL(12,2) NULL,
   `costoNormal` DECIMAL(12,2) NULL,
   `costoAumento` DECIMAL(12,2) NOT NULL,
   `descuento` INT NULL,
   `asesores` VARCHAR(90) NULL,
-  `Condicion` TINYINT NULL,
+  `condicion` TINYINT NULL,
   `cliente_idCliente` INT NOT NULL,
-  PRIMARY KEY (`idCotizacion`, `costoAumento`),
+  PRIMARY KEY (`idCotizacion`),
   INDEX `fk_cotizacion_cliente1_idx` (`cliente_idCliente` ASC),
   CONSTRAINT `fk_cotizacion_cliente1`
     FOREIGN KEY (`cliente_idCliente`)
@@ -190,7 +190,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`empleado`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`empleado` (
-  `idEmpleado` INT NOT NULL,
+  `idEmpleado` INT NOT NULL AUTO_INCREMENT,
   `identificador` VARCHAR(10) NULL,
   `nombre` VARCHAR(200) NULL,
   `direccion` VARCHAR(200) NULL,
@@ -204,10 +204,10 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`area`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`area` (
-  `idarea` INT NOT NULL,
+  `idArea` INT NOT NULL AUTO_INCREMENT,
   `descripcion` VARCHAR(90) NULL,
   `condicion` TINYINT NULL,
-  PRIMARY KEY (`idarea`))
+  PRIMARY KEY (`idArea`))
 ENGINE = InnoDB;
 
 
@@ -215,11 +215,11 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`usuarioInt`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`usuarioInt` (
-  `idusuarioInt` INT NOT NULL,
+  `idUsuarioInt` INT NOT NULL AUTO_INCREMENT,
   `password` VARCHAR(90) NULL,
   `usuario` VARCHAR(120) NULL,
   `condicion` TINYINT NULL,
-  PRIMARY KEY (`idusuarioInt`))
+  PRIMARY KEY (`idUsuarioInt`))
 ENGINE = InnoDB;
 
 
@@ -227,21 +227,21 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`permiso`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`permiso` (
-  `idpermiso` INT NOT NULL,
+  `idPermiso` INT NOT NULL AUTO_INCREMENT,
   `condicion` TINYINT NULL,
-  `area_idarea` INT NOT NULL,
-  `usuarioInt_idusuarioInt` INT NOT NULL,
-  PRIMARY KEY (`idpermiso`),
-  INDEX `fk_permiso_area1_idx` (`area_idarea` ASC),
-  INDEX `fk_permiso_usuarioInt1_idx` (`usuarioInt_idusuarioInt` ASC),
+  `area_idArea` INT NOT NULL,
+  `usuarioInt_idUsuarioInt` INT NOT NULL,
+  PRIMARY KEY (`idPermiso`),
+  INDEX `fk_permiso_area1_idx` (`area_idArea` ASC),
+  INDEX `fk_permiso_usuarioInt1_idx` (`usuarioInt_idUsuarioInt` ASC),
   CONSTRAINT `fk_permiso_area1`
-    FOREIGN KEY (`area_idarea`)
-    REFERENCES `Audamex`.`area` (`idarea`)
+    FOREIGN KEY (`area_idArea`)
+    REFERENCES `Audamex`.`area` (`idArea`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_permiso_usuarioInt1`
-    FOREIGN KEY (`usuarioInt_idusuarioInt`)
-    REFERENCES `Audamex`.`usuarioInt` (`idusuarioInt`)
+    FOREIGN KEY (`usuarioInt_idUsuarioInt`)
+    REFERENCES `Audamex`.`usuarioInt` (`idUsuarioInt`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -251,7 +251,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`usuarioExt`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`usuarioExt` (
-  `idUsuarioExt` INT NOT NULL,
+  `idUsuarioExt` INT NOT NULL AUTO_INCREMENT,
   `usuario` VARCHAR(90) NULL,
   `pasword` VARCHAR(90) NULL,
   `condicion` TINYINT NULL,
@@ -270,7 +270,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`solicitud`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`solicitud` (
-  `idSolicitud` INT NOT NULL,
+  `idSolicitud` INT NOT NULL AUTO_INCREMENT,
   `folio` VARCHAR(5) NULL,
   `fecha` DATE NULL,
   `observaciones` LONGTEXT NULL,
@@ -291,11 +291,11 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`servicio`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`servicio` (
-  `idservicio` INT NOT NULL,
+  `idServicio` INT NOT NULL AUTO_INCREMENT,
   `descripcion` VARCHAR(120) NULL,
   `observaciones` LONGTEXT NULL,
   `condicion` TINYINT NULL,
-  PRIMARY KEY (`idservicio`))
+  PRIMARY KEY (`idServicio`))
 ENGINE = InnoDB;
 
 
@@ -303,7 +303,7 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`certificacionVsNorma`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`certificacionVsNorma` (
-  `idCertificacionVsNorma` INT NOT NULL,
+  `idCertificacionVsNorma` INT NOT NULL AUTO_INCREMENT,
   `certificacion_idCertificacion` INT NOT NULL,
   `norma_idNorma` INT NOT NULL,
   PRIMARY KEY (`idCertificacionVsNorma`),
@@ -326,9 +326,9 @@ ENGINE = InnoDB;
 -- Table `Audamex`.`solicitudVsServicios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Audamex`.`solicitudVsServicios` (
-  `idSolicitudVsServicios` INT NOT NULL,
+  `idSolicitudVsServicios` INT NOT NULL AUTO_INCREMENT,
   `solicitud_idSolicitud` INT NOT NULL,
-  `servicio_idservicio` INT NOT NULL,
+  `servicio_idServicio` INT NOT NULL,
   PRIMARY KEY (`idSolicitudVsServicios`),
   INDEX `fk_solicitudVsServicios_solicitud1_idx` (`solicitud_idSolicitud` ASC),
   INDEX `fk_solicitudVsServicios_servicio1_idx` (`servicio_idservicio` ASC),
@@ -338,8 +338,8 @@ CREATE TABLE IF NOT EXISTS `Audamex`.`solicitudVsServicios` (
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_solicitudVsServicios_servicio1`
-    FOREIGN KEY (`servicio_idservicio`)
-    REFERENCES `Audamex`.`servicio` (`idservicio`)
+    FOREIGN KEY (`servicio_idServicio`)
+    REFERENCES `Audamex`.`servicio` (`idServicio`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
